8844bd8d9e68cdd078c7148b41ef26450f45ffb2
AdrianRodriguezJorge <139442845+AdrianRodriguezJorge@users.noreply.github.com>
2024-11-02 18:19:47 +0000

    6.19

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Añade un efecto de suavizado al desplazarse entre secciones
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
