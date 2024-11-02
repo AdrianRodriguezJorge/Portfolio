9d0544dcaa313faf059dd84e038ae130132101ae
AdrianRodriguezJorge <139442845+AdrianRodriguezJorge@users.noreply.github.com>
2024-11-02 15:09:36 +0000

    3.09pm

---
+++ script.js
@@ -0,0 +1,9 @@
+// Añade un efecto de suavizado al desplazarse entre secciones
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
